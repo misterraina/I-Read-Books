bf9ecdee7363dfef7e0be357784f091298dd6e17
misterraina <ayushraina617@gmail.com>
2024-12-11 00:13:39 +0530

    pg dilect

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import pg from 'pg'
 
 dotenv.config();
 
@@ -7,6 +8,7 @@ const sequelize = new Sequelize(
     process.env.DB_LINK, {
       dialect: 'postgres', 
       logging:false, // Logs queries in non-production environments
+      dialectModule: pg
     } ) 
 
 export default sequelize;
