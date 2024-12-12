15a3726da514adccc189dccb7335c7c517a61206
misterraina <ayushraina617@gmail.com>
2024-12-13 01:19:39 +0530

    chal backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the output directory matches Vercel's static deployment
-    emptyOutDir: true, // Clears the output directory before building
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000', // Proxy API calls during development
-    },
-  },
-});
+})
