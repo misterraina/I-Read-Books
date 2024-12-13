179eb7013c6f5f9671417d9bd0421ec016d50702
misterraina <ayushraina617@gmail.com>
2024-12-13 14:51:00 +0530

    cookie domain

--- client/src/const.js
+++ client/src/const.js
@@ -1,5 +1,5 @@
-export const REACT_APP_API_BACKEND="https://i-read-books-backend.vercel.app"
-// export const REACT_APP_API_BACKEND="http://localhost:3000"
+// export const REACT_APP_API_BACKEND="https://i-read-books-backend.vercel.app"
+export const REACT_APP_API_BACKEND="http://localhost:3000"
 
 
 
