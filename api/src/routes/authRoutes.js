b32a72ed34f144f831fcf373d70dda27636a15f0
misterraina <ayushraina617@gmail.com>
2024-12-13 15:28:40 +0530

    secure false

--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -55,7 +55,7 @@ authRoute.post('/login', async (req, res) => {
         // Set token in an HTTP-only cookie
         res.cookie('adminToken', token, {
             httpOnly: false,
-            secure: true,
+            secure: false,
             maxAge: 90 * 60 * 1000, // 90 minutes
             // domain:"i-read-books.vercel.app",
             sameSite: 'None', // Adjust depending on your frontend-backend setup
@@ -91,7 +91,7 @@ authRoute.post('/logout', (req, res) => {
     // Clear the cookie by setting its expiration to the past
     res.clearCookie('adminToken', {
         httpOnly: false,
-        secure: true,
+        secure: false,
         sameSite: 'None', // Adjust SameSite as per your app's needs
         path: '/', // Match the path of the cookie
     });
